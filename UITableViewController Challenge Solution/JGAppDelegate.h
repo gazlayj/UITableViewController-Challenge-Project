bbe63425eb4c2e251e681b274234d0ed1517e2fd
Justin Gazlay <justingazlay@Justin-Gazlays-iMac.local>
2014-06-03 22:29:56 -0600

    Initial Commit

---
+++ UITableViewController Challenge Solution/JGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JGAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Justin Gazlay on 6/3/14.
+//  Copyright (c) 2014 Justin Gazlay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Justin Gazlay on 6/3/14.
+//  Copyright (c) 2014 Justin Gazlay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JGAppDelegate class]));
+    }
+}
