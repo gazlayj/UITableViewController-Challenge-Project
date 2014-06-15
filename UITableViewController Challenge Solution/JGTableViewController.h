a20a12511358322b96aade3cbcd3cf9f600e661b
Justin Gazlay <justingazlay@Justin-Gazlays-iMac.local>
2014-06-14 21:59:50 -0600

    solved the challenge

---
+++ UITableViewController Challenge Solution/JGTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  JGTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Justin Gazlay on 6/14/14.
+//  Copyright (c) 2014 Justin Gazlay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *data;
+
+@end
---
+++ UITableViewController Challenge Solution/JGTableViewController.m
@@ -0,0 +1,153 @@
+//
+//  JGTableViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Justin Gazlay on 6/14/14.
+//  Copyright (c) 2014 Justin Gazlay. All rights reserved.
+//
+
+#import "JGTableViewController.h"
+
+@interface JGTableViewController ()
+
+@end
+
+@implementation JGTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    
+    self.data = [[NSMutableArray alloc] init];
+    NSString *section1 = @"I am in section 0";
+    NSString *section2 = @"another section";
+    NSString *section3 = @"cell";
+    
+    [self.data addObject:section1];
+    [self.data addObject:section2];
+    [self.data addObject:section3];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+
+    // Return the number of sections.
+    return 3;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+
+    if (section == 0) {
+        return 2;
+    }
+    else if (section == 1) {
+        return 1;
+    }
+    else if (section == 2) {
+        return 3;
+    }
+    else {
+        return 0;
+    }
+    
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    if (indexPath.section == 2) {
+        
+       
+        cell.textLabel.text = [NSString stringWithFormat:@"%@ %i", [self.data objectAtIndex:indexPath.section], indexPath.row];
+    }
+    else
+    {
+        
+        cell.textLabel.text = [NSString stringWithFormat:@"%@", [self.data objectAtIndex:indexPath.section]];
+    }
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
